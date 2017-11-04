80a76277df6bf0420e788136481dbc4c4035e402
Markus Fält <markus.falt@gmail.com>
2017-11-04 21:49:04 +0100

    Skapad singleton klass för att hantera fönster.

--- code/main.cpp
+++ code/main.cpp
@@ -1,39 +1,12 @@
 #include <SDL2/SDL.h>
 #include <stdlib.h>
+#include "Display.h"
 
 int main(int argc, char** argv)
 {
-	SDL_Init(SDL_INIT_EVERYTHING); // Sätter upp SDL
-
-	SDL_Window* window = SDL_CreateWindow // Skapar ett fönster
-	(
-		"SDL window",
-		SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED,
-		640, 480, 0
-	);
-
-	SDL_Renderer* renderer = SDL_CreateRenderer // Skapar en renderare
-	(
-		window, -1, 0
-	);
-
-	SDL_Texture* texture = SDL_CreateTexture // Skapar en textur
-	(
-		renderer,
-		SDL_PIXELFORMAT_ARGB8888,
-		SDL_TEXTUREACCESS_STREAMING,
-		640, 480
-	);
-
-	unsigned char screenBuffer[640*480*4]; // En buffer för alla pixlar
-	int screenBufferPitch = 640*4;
-
-	// Slumpa färger till alla pixlar
-	for(int i = 0; i < 640*480*4; i++)
-	{
-		screenBuffer[i] = (unsigned char)(rand() % 256);
-	}
+	Display::setWidth(640);
+	Display::setHeight(480);
+	Display::get();
 
 	bool running = true;
 
@@ -47,22 +20,8 @@ int main(int argc, char** argv)
 				running = false;
 			}
 		}
-		// Sätt texturen från en lista med pixel data
-		SDL_UpdateTexture(
-			texture, 0,
-			screenBuffer, screenBufferPitch);
-		// Kopiera texturen till renderaren
-		SDL_RenderCopy(renderer, texture, 0, 0);
-		// Presentera det som är renderaren
-		SDL_RenderPresent(renderer);
+		Display::get().update();
 	}
-
-	// Tar bort allt
-	SDL_DestroyTexture(texture);
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-
-	SDL_Quit();
 
 	return 0;
 }
