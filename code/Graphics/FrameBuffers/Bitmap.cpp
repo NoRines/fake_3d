fdaab14a2e7166896368bd54da4897554115b1c4
Markus Fält <markus.falt@gmail.com>
2017-11-05 01:07:05 +0100

    La till bitmap klass.

---
+++ code/Graphics/FrameBuffers/Bitmap.cpp
@@ -0,0 +1,79 @@
+#include "Bitmap.h"
+#include <algorithm>
+
+#include <iostream>
+
+Bitmap::Bitmap(int width, int height):
+	width(width), height(height),
+	buffer(new uint32_t[width*height])
+{
+}
+
+Bitmap::Bitmap(const Bitmap& bitmap):
+	width(bitmap.width), height(bitmap.height),
+	buffer(new uint32_t[bitmap.width*bitmap.height])
+{
+	std::copy
+	(
+		bitmap.buffer,
+		bitmap.buffer + (width*height),
+		buffer
+	);
+}
+
+Bitmap& Bitmap::operator=(const Bitmap& bitmap)
+{
+	Bitmap temp = bitmap;
+	std::swap(temp.width, width);
+	std::swap(temp.height, height);
+	std::swap(temp.buffer, buffer);
+	return *this;
+}
+
+Bitmap::Bitmap(Bitmap&& bitmap):
+	width(bitmap.width), height(bitmap.height),
+	buffer(bitmap.buffer)
+{
+	bitmap.buffer = nullptr;
+	std::cout << "Här" << std::endl;
+}
+
+Bitmap& Bitmap::operator=(Bitmap&& bitmap)
+{
+	std::swap(bitmap.width, width);
+	std::swap(bitmap.height, height);
+	std::swap(bitmap.buffer, buffer);
+	std::cout << "Härhar" << std::endl;
+	return *this;
+}
+
+Bitmap::~Bitmap()
+{
+	delete[] buffer;
+}
+
+int Bitmap::getWidth() const
+{
+	return width;
+}
+
+int Bitmap::getHeight() const
+{
+	return height;
+}
+
+void Bitmap::clear(uint32_t color)
+{
+	std::fill(buffer, buffer + (width*height), color);
+}
+
+void Bitmap::copyToByteBuffer(uint8_t* byteBuffer) const
+{
+	uint32_t* tmpPtr = (uint32_t*)byteBuffer;
+	std::copy
+	(
+		buffer,
+		buffer + (width*height),
+		tmpPtr
+	);
+}
---
+++ code/Graphics/FrameBuffers/Bitmap.h
@@ -0,0 +1,35 @@
+#ifndef BITMAP_H
+#define BITMAP_H
+
+#include <stdint.h>
+
+class Bitmap
+{
+public:
+	Bitmap() = delete;
+
+	Bitmap(int width, int height);
+
+	Bitmap(const Bitmap& bitmap);
+	Bitmap& operator=(const Bitmap& bitmap);
+
+	Bitmap(Bitmap&& bitmap);
+	Bitmap& operator=(Bitmap&& bitmap);
+
+	virtual ~Bitmap();
+
+	int getWidth() const;
+	int getHeight() const;
+
+	void clear(uint32_t color);
+
+	void copyToByteBuffer(uint8_t* byteBuffer) const;
+
+private:
+	int width;
+	int height;
+	// AARRGGBB
+	uint32_t* buffer;
+};
+
+#endif
